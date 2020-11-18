47f423f0ac62d152f905cdb3cda0180ea17a789f
dariahazaparu <61266022+dariahazaparu@users.noreply.github.com>
2020-11-19 00:07:12 +0300

    Add project files.

---
+++ OpenDiscussionPlatform/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OpenDiscussionPlatform.MvcApplication" Language="C#" %>
---
+++ OpenDiscussionPlatform/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OpenDiscussionPlatform.Startup))]
+namespace OpenDiscussionPlatform
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
